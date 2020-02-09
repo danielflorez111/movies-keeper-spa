8810a7bf6e30009b4f293f2c194a8ac6ea1e1496
danielflorez111 <danielflorez@Daniels-MacBook-Pro.local>
2020-02-09 18:19:28 -0500

    Adding semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Button>Click Here</Button>
+      <Button content='Primary' primary />
+      <Button content='Secondary' secondary />
     </div>
   );
 }
